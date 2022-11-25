6bccdc7d7276fcb076f482860e38f7d893547deb
Michał Madeja <35845468+michal7954@users.noreply.github.com>
2022-11-25 19:56:01 +0100

    Lab7: Obliczanie macierzy HG

--- src/classes/Grid.py
+++ src/classes/Grid.py
@@ -1,8 +1,11 @@
+from params.consts import nodeSize
+
 class Grid:
     nodesNumber = None
     elementsNumber = None
     nodes = []
     elements = []
+    HG = []
 
     def print(self):
         print(f'Nodes number: {self.nodesNumber}')
@@ -16,3 +19,20 @@ class Grid:
         print('Elements:')
         for element in self.elements:
             print(element)
+
+    def calculateHG(self):
+        self.HG = [[0 for _ in range(len(self.nodes))] for _ in range(len(self.nodes))]
+
+        for element in self.elements:
+            for i in range(nodeSize):
+                for j in range(nodeSize):
+                    self.HG[element.nodes[i].id - 1][element.nodes[j].id - 1] += element.HTotal[i][j]
+
+    def printHG(self):
+        print('HG')
+        for row in self.HG:
+            rowPrintable = ''
+            for element in row:
+                rowPrintable += "{:8.2f}".format(element)
+            print(rowPrintable)
+        print()
